41f193154717f8a97cba5c9c517993e3526a3a2a
ajay-makvana <ajaymakvana2001@gmail.com>
2021-03-19 17:03:25 +0530

    Initial Upload

--- EcomWeb/EcomWeb/settings.py
+++ EcomWeb/EcomWeb/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')29gxaq4d+p7galqkg@^0ecac@kd9jr6jz2kyc6$&s!yy7(-f4'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
